bbeb4016a379870de7027707c148bac9249aa988
HamzaA598 <hamzaemad598@gmail.com>
2023-11-25 13:34:34 +0200

    add: implemented initialize

--- GA.py
+++ GA.py
@@ -1,14 +1,24 @@
-# inputs: population size, chromosome length
+import numpy as np
+
+POP_SIZE = 8
+K = 0.2*POP_SIZE
+
+# inputs: chromosome length
 # output: (list/2d numpy array) of numpy arrays where each inner array is
 # a chromosome (coeffieicents)
-def initalize():
-    pass
+def initialize(chromosome_length):
+
+    initial_population = np.empty((POP_SIZE, chromosome_length))
+    for i in range(POP_SIZE):
+        initial_population[i, :] = np.random.rand(chromosome_length);
+    
+    return initial_population
 
 # inputs: population, data pojnts
-# ouput: (list/numpy array) of fitness_values 
+# ouput: (list/numpy array) of fitness_values
 def fitness():
     pass
- 
+
 # inputs: population, fitness_values, size of tournament?
 # output: selected parents
 def selection():
---
+++ unit_testing.py
@@ -0,0 +1,3 @@
+from GA import initialize
+
+print(initialize(6))
